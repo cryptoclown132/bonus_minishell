1c1d7c42b5c666a2206d69d46f81d22b5e0843de
sachmull <sachmull@1-B-27.42heilbronn.de>
2025-08-26 19:58:42 +0200

    fixed: norminette

--- lexer_expander/init_token.c
+++ lexer_expander/init_token.c
@@ -52,8 +52,8 @@ char	*itw_loop(char *input, char *tkn, int *j, char **envp)
 	}
 	if (t.tmp != '\'')
   t.tmp_str = expander(t.tmp_str, envp);
-  if (t.tmp != '\'' && t.tmp != '"' && ft_strchr(t.tmp_str, '*'))
-    t.tmp_str = wildcard(t.tmp_str);
+	if (t.tmp != '\'' && t.tmp != '"' && ft_strchr(t.tmp_str, '*'))
+		t.tmp_str = wildcard(t.tmp_str);
 	tkn = free_both_strjoin(tkn, t.tmp_str);	
 	*j = t.k + 1;
 	return (tkn);
--- lexer_expander/wildcard.c
+++ lexer_expander/wildcard.c
@@ -3,81 +3,74 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-#include <stdio.h>
-
 bool	path_matches(char *pattern, char *path, bool wildcard_active)
 {
 	if (*pattern == '*')
-	  return (path_matches(pattern + 1, path, true));
-
-  // test for '\0'
+		return (path_matches(pattern + 1, path, true));
 	if ((*pattern == '\0') && (*path == '\0'))
 		return (true);
 	if (*pattern == '\0' && *path != '\0' && !wildcard_active)
 		return (false);
 	if (*pattern == '\0' && *path != '\0' && wildcard_active)
-    	return (true);
+		return (true);
 	if (*pattern != '\0' && *path == '\0')
 		return (false);
-
-  if (*pattern == *path && !wildcard_active)
-    return(path_matches(pattern + 1, path + 1, false));
-  if (*pattern == *path && wildcard_active)
-  {
-    if (path_matches(pattern + 1, path + 1, false) == false)
-    {
-      return (path_matches(pattern, path + 1, true));
-    } 
-    else
-      return (true);
-  }
-
-  if (*pattern != *path && wildcard_active)
-    return(path_matches(pattern, path + 1, true));
-  if (*pattern != *path && !wildcard_active)
-    return(false);
-  
-  printf("Error in wildcard\n");
-  return (true);
+	if (*pattern == *path && !wildcard_active)
+		return (path_matches(pattern + 1, path + 1, false));
+	if (*pattern == *path && wildcard_active)
+	{
+		if (path_matches(pattern + 1, path + 1, false) == false)
+			return (path_matches(pattern, path + 1, true));
+		else
+			return (true);
+	}
+	if (*pattern != *path && wildcard_active)
+		return (path_matches(pattern, path + 1, true));
+	if (*pattern != *path && !wildcard_active)
+		return (false);
+	return (true);
 }
 
-void  append_path(char **matching_paths, char *to_append)
+void	append_path(char **matching_paths, char *to_append)
 {
-  char  *tmp;
+	char	*tmp;
 
-  if (ft_strlen(*matching_paths) != 0)
-  {
-    tmp = *matching_paths;
-    *matching_paths = ft_strjoin(*matching_paths, " ");
-    free(tmp);
-  }
-  tmp = *matching_paths;
-  *matching_paths = ft_strjoin(*matching_paths, to_append);
-  free(tmp);
+	if (ft_strlen(*matching_paths) != 0)
+	{
+		tmp = *matching_paths;
+		*matching_paths = ft_strjoin(*matching_paths, " ");
+		free(tmp);
+	}
+	tmp = *matching_paths;
+	*matching_paths = ft_strjoin(*matching_paths, to_append);
+	free(tmp);
 }
 
-char  *wildcard(char *pattern)
+char	*wildcard(char *pattern)
 {
-  DIR           *dirp;
-  struct dirent *dirent;
-  char          *matching_paths;
-  const bool    match_all = ft_strlen(pattern) == 1 && *pattern == '*';
-  const bool    match_hidden_files = pattern[0] == '.';
+	DIR				*dirp;
+	struct dirent	*dirent;
+	char			*matching_paths;
+	const bool		match_all = ft_strlen(pattern) == 1 && *pattern == '*';
+	const bool		match_hidden_files = pattern[0] == '.';
 
-  matching_paths = ft_calloc(1, 1);
-  dirp = opendir(".");
-  while ((dirent = readdir(dirp)))
-  {
-    if ((match_all && dirent->d_name[0] != '.')
-      || (dirent->d_name[0] == '.' && match_hidden_files && path_matches(pattern, dirent->d_name, false))
-      || (path_matches(pattern, dirent->d_name, false) && dirent->d_name[0]!= '.'))
-      append_path(&matching_paths, dirent->d_name);
-  }
-
-  if (ft_strlen(matching_paths) == 0)
-  {
-    free(matching_paths);
-    return (pattern);
-  }
-  return (matching_paths);
+	matching_paths = ft_calloc(1, 1);
+	dirp = opendir(".");
+	dirent = readdir(dirp);
+	while (dirent || closedir(dirp))
+	{
+		if ((match_all && dirent->d_name[0] != '.')
+			|| (dirent->d_name[0] == '.' && match_hidden_files && path_matches
+				(pattern, dirent->d_name, false)) || (path_matches
+				(pattern, dirent->d_name, false) && dirent->d_name[0] != '.'))
+			append_path(&matching_paths, dirent->d_name);
+		dirent = readdir(dirp);
+	}
+	if (ft_strlen(matching_paths) == 0)
+	{
+		free(matching_paths);
+		return (pattern);
+	}
+	free(pattern);
+	return (matching_paths);
 }
